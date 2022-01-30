a133a8d253a89110f88f4a81396063b8d032dbcd
FrostTaco <puppyloganpuppy@gmail.com>
2022-01-30 16:25:42 -0500

    Removed pointless listener implementation

--- src/main/java/net/iceblaze/ScoreboardTest/ScoreboardTest.java
+++ src/main/java/net/iceblaze/ScoreboardTest/ScoreboardTest.java
@@ -5,10 +5,9 @@ import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
-import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class ScoreboardTest extends JavaPlugin implements Listener {
+public class ScoreboardTest extends JavaPlugin {
 
     public void onEnable(){
     }
