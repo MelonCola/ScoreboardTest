3d753130388a3fce0f90fc27881634b783a4d475
FrostTaco <puppyloganpuppy@gmail.com>
2022-01-30 16:20:58 -0500

    Initial Commit

---
+++ src/main/java/net/iceblaze/ScoreboardTest/ScoreboardTest.java
@@ -0,0 +1,32 @@
+package net.iceblaze.ScoreboardTest;
+
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.event.Listener;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class ScoreboardTest extends JavaPlugin implements Listener {
+
+    public void onEnable(){
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if(command.getName().equalsIgnoreCase("toggleSB")) {
+            if(sender instanceof Player) {
+                Player p = (Player) sender;
+                if(p.getScoreboard() == Bukkit.getScoreboardManager().getMainScoreboard()) {
+                    p.setScoreboard(Bukkit.getScoreboardManager().getNewScoreboard());
+                    p.sendMessage(ChatColor.BLUE + "Now on a custom scoreboard");
+                } else {
+                    p.setScoreboard(Bukkit.getScoreboardManager().getMainScoreboard());
+                    p.sendMessage(ChatColor.BLUE + "Now on the default scoreboard");
+                }
+            }
+        }
+        return true;
+    }
+}
